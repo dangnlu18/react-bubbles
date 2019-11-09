57cc7e0f6d987a06e856937b0450302db01df4b7
dang lu <danglu@dangs-mbp.lan>
2019-11-08 17:16:05 -0800

    MVP and stretch

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import api from '../utils/api';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(()=>{
+  	api().get('/colors')
+  		.then((resp) => {setColorList(resp.data)})
+  		.catch((err)=> console.log(err))
+  },[])
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import api from '../utils/api'
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+	const [error, setError] = useState()
+
+	const [ data, setData ] = useState({
+		username:'',
+		password:''
+	})
+
+	const handleChange = e =>{
+		setData({
+			...data, 
+			[e.target.name]: e.target.value
+		})
+	}
+
+	const handleSubmit = e =>{
+		e.preventDefault()
+
+		api()
+			.post('/login', data)
+			.then((res)=>{
+				localStorage.setItem("token",res.data.payload);
+				props.history.push('/bubbles')
+			})
+			.catch((err)=>{
+				setError(err.response.data.message)
+			})
+
+	}
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+  	<div className='login'>
+  		<h2> Sign In </h2>
+  		<form onSubmit ={handleSubmit}>
+  			{error && <div className='error'>{error}</div>}
+  			<input type='username' name='username' placeholder='username' value={data.username} onChange ={handleChange} /> <br/>
+  			<input type='password' name='password' placeholder='password' value={data.password} onChange ={handleChange} /><br/>
+
+  			<button type='submit'> Login </button>
+  		</form>
+  	</div>
   );
 };
 
